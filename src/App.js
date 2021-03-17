91a069cb2dd096dad510cca4e5e89afd4be59167
shinji0320 <shinji.blue.one@gmail.com>
2021-03-18 07:59:16 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(count)
+  }, [count])
+
+  return (
+    <div>
+      <p>You clicked { count } times.</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  )
+}
+
+export default App;
