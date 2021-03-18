dc53b64b12580319ac14d010c17acd3af1ebb209
shinji0320 <shinji.blue.one@gmail.com>
2021-03-18 09:00:57 +0900

    APIの取得確認しました

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
+  const [text, setText] = useState('');
+  const [query, setQuery] = useState('apple');
   useEffect(() => {
-    console.log(count)
-  }, [count])
+    console.log('useEffectが走りました')
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${process.env.REACT_APP_CLIENT_ID}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setImages(data.results)
+      })
+  }, [query])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setQuery(text);
+    setText('');
+    console.log("onSubmitが呼ばれました")
+  }
 
   return (
-    <div>
-      <p>You clicked { count } times.</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
+    <div className="App">
+      <div className="main">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            onChange={e => setText(e.target.value)}
+            value={text}
+          />
+          <button type="submit">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
